24340bc831b3ca60700da7f0ee2551cf22e81efa
guilhermesiqueira13 <guilherme.nunes13@fatec.sp.gov.br>
2025-04-22 16:06:39 -0300

    final da navegação prev e nav

--- js/slide.js
+++ js/slide.js
@@ -3,14 +3,19 @@ export default class Slide {
     this.slide = document.querySelector(slide);
     this.wrap = document.querySelector(wrap);
     this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+    this.index = { prev: null, active: 0, next: null };
+  }
+
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s" : "";
   }
 
   moveSlide(distX) {
-    this.dist.movePostion = distX;
+    this.dist.movePosition = distX; // Fixed typo
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
   }
 
-  updatePostion(clientX) {
+  updatePosition(clientX) {
     this.dist.movement = (this.dist.startX - clientX) * 1.6;
     return this.dist.finalPosition - this.dist.movement;
   }
@@ -33,14 +38,27 @@ export default class Slide {
       event.type === "mousemove"
         ? event.clientX
         : event.changedTouches[0].clientX;
-    const finalPosition = this.updatePostion(pointerPosition);
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
+    this.transition(false);
   }
 
   onEnd(event) {
     const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
     this.wrap.removeEventListener(moveType, this.onMove);
-    this.dist.finalPosition = this.dist.movePostion;
+    this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== null) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== null) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
   addSlideEvents() {
@@ -56,38 +74,57 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
-  // Slides config
-  slidePostition(slide) {
-    const margin = this.wrap.offsetwidth - slide.offsetwidth / 2;
-    return -(slide.offsetleft - margin);
+  slidePosition(slide) {
+    const margin = (this.wrap.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
   }
 
   slideConfig() {
     this.slideArray = [...this.slide.children].map((element) => {
-      const position = this.slidePostition(element);
+      const position = this.slidePosition(element);
       return { position, element };
     });
+    this.slidesIndexNav(0);
   }
 
   slidesIndexNav(index) {
     const last = this.slideArray.length - 1;
     this.index = {
-      prev: index ? index - 1 : null,
+      prev: index > 0 ? index - 1 : null,
       active: index,
-      next: index === last ? null : index + 1,
+      next: index < last ? index + 1 : null,
     };
   }
 
   changeSlide(index) {
     const activeSlide = this.slideArray[index];
-    this.moveSlide(this.slideConfig[index].position);
-    this.slidesIndexNav(index);
-    this.dist.finalPosition = activeSlide.position;
+    if (activeSlide) {
+      this.moveSlide(activeSlide.position);
+      this.slidesIndexNav(index);
+      this.dist.finalPosition = activeSlide.position;
+    } else {
+      console.error("Invalid slide index:", index);
+    }
+  }
+
+  activePrevSlide() {
+    if (this.index.prev !== null) {
+      this.changeSlide(this.index.prev);
+    }
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== null) {
+      this.changeSlide(this.index.next);
+    }
   }
 
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
+    this.slideConfig();
+
     return this;
   }
 }
